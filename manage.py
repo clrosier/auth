1e9506f2732dd6a283a1a35cf7b4fc691c3a6002
Cameron Rosier <cameron.rosier@ryerson.ca>
2020-06-12 17:08:39 -0400

    trying to add a postgres exporter sidecar

--- manage.py
+++ manage.py
@@ -9,6 +9,7 @@ from app.main import create_app, db
 from app.main.model import user
 from app.main.model import blacklist
 
+
 app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
 app.register_blueprint(blueprint)
 
